5ec4794211ec96f5473100eb7f9271ab485efda8
Mike <mike.wildcats@gmail.com>
2018-12-19 08:57:15 -0500

    Bumbling about building guilds

--- src/main/java/com/everneth/rp/commands/GuildCommand.java
+++ src/main/java/com/everneth/rp/commands/GuildCommand.java
@@ -1,4 +1,31 @@
 package com.everneth.rp.commands;
 
-public class GuildCommand {
+import co.aikar.commands.BaseCommand;
+import co.aikar.commands.annotation.CommandAlias;
+import co.aikar.commands.annotation.CommandPermission;
+import co.aikar.commands.annotation.Subcommand;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+@CommandAlias("guild")
+public class GuildCommand extends BaseCommand {
+    @Subcommand("guild create")
+    @CommandPermission("emi.rp.guild")
+    public void onGuildPlayerCreate(CommandSender sender, String name, Player p2, Player p3)
+    {
+        if(!p2.isOnline() || !p3.isOnline())
+        {
+            sender.sendMessage("One or more players offline! If you need assistance, please contact a GM.");
+        }
+        else
+        {
+            createGuild(name, p2, p3);
+        }
+    }
+
+    protected void createGuild(String name, Player p1, Player p2, Player p3 = null)
+    {
+
+    }
+
 }
--- src/main/java/com/everneth/rp/models/Guild.java
+++ src/main/java/com/everneth/rp/models/Guild.java
@@ -1,4 +1,38 @@
 package com.everneth.rp.models;
 
 public class Guild {
+    private int id;
+    private String name;
+    private int leaderId;
+
+    public Guild(int id, String name, int leaderId)
+    {
+        this.id = id;
+        this.name = name;
+        this.leaderId = leaderId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLeaderId() {
+        return leaderId;
+    }
+
+    public void setLeaderId(int leaderId) {
+        this.leaderId = leaderId;
+    }
 }
