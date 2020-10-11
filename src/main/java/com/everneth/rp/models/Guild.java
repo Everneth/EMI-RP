4c1d5e5f1a9d7eb25d357d84a24b9cc9dea3cb7d
Mike White <mike.wildcats@gmail.com>
2020-10-10 21:42:58 -0400

    Guild refactor pt 2

--- src/main/java/com/everneth/rp/models/Guild.java
+++ src/main/java/com/everneth/rp/models/Guild.java
@@ -1,7 +1,10 @@
 package com.everneth.rp.models;
 
+import co.aikar.idb.DB;
+import com.everneth.rp.RP;
 import org.bukkit.entity.Player;
 
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
@@ -15,6 +18,9 @@ public class Guild {
     private boolean isRp;
     private String createdDate;
     private int tier;
+
+
+    public Guild(){}
 
     public Guild(String name, Player commandSender, String primaryColor, String secondaryColor, boolean isRp)
     {
@@ -32,6 +38,101 @@ public class Guild {
         this.tier = 1;
     }
 
+    private int createGuild(Guild guild)
+    {
+        try {
+            DB.executeInsert(
+                    "INSERT INTO guilds (guild_name, guild_leader_id, guild_score, guild_primary_color," +
+                            " guild_secondary_color, guild_banner_path, guild_is_rp, guild_created_date, guild_tier) " +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)",
+                    guild.getName(),
+                    guild.getLeaderId(),
+                    guild.getScore(),
+                    guild.getPrimaryColor(),
+                    guild.getSecondaryColor(),
+                    null,
+                    guild.isRp(),
+                    guild.getCreatedDate(),
+                    guild.getTier()
+            );
+        }
+        catch (SQLException e)
+        {
+            RP.getPlugin().getLogger().warning(e.getMessage());
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getLeaderId() {
+        return leaderId;
+    }
+
+    public void setLeaderId(int leaderId) {
+        this.leaderId = leaderId;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getPrimaryColor() {
+        return primaryColor;
+    }
+
+    public void setPrimaryColor(String primaryColor) {
+        this.primaryColor = primaryColor;
+    }
+
+    public String getSecondaryColor() {
+        return secondaryColor;
+    }
+
+    public void setSecondaryColor(String secondaryColor) {
+        this.secondaryColor = secondaryColor;
+    }
+
+    public String getBannerPath() {
+        return bannerPath;
+    }
+
+    public void setBannerPath(String bannerPath) {
+        this.bannerPath = bannerPath;
+    }
+
+    public boolean isRp() {
+        return isRp;
+    }
+
+    public void setRp(boolean rp) {
+        isRp = rp;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getTier() {
+        return tier;
+    }
+
+    public void setTier(int tier) {
+        this.tier = tier;
+    }
 
     private int getLeaderPlayerId(Player leader)
     {
