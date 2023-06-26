2e8baa17032a64ef665d5c563c0084f851ca5949
Mike White <mike.wildcats@gmail.com>
2023-06-26 09:12:05 -0500

    Add Character and Season models. needs CRUD and commands

--- src/main/java/com/everneth/rp/models/Character.java
+++ src/main/java/com/everneth/rp/models/Character.java
@@ -1,5 +1,12 @@
 package com.everneth.rp.models;
 
 public class Character {
-    // TODO: DB Table
+    private int id;
+    private int playerId;
+    private int level;
+    private long exp;
+    private String name;
+    private int classId;
+    private String dateCreated;
+    private String dateRetired;
 }
--- src/main/java/com/everneth/rp/models/RPSeason.java
+++ src/main/java/com/everneth/rp/models/RPSeason.java
@@ -1,5 +1,13 @@
 package com.everneth.rp.models;
 
 public class RPSeason {
-    //TODO: DB Table
+    private int id;
+    private String name;
+    private int seasonTypeId;
+    private int seasonThemeId;
+    private boolean allowGuilds;
+    private String dateCreated;
+    private String dateStarted;
+    private String dateEnded;
+    private int resultId;
 }
